868f07408f72eceb86fc28cdfc8936e12d6abe7e
marcuspereiragithub <vinnycoast@gmail.com>
2019-04-07 19:33:49 -0300

    Connection with db

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const app = express();
 
+const sqlite = require('sqlite');
+const dbConnection = sqlite.open('banco.sqlite', { Promise });
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (request, response) => {
-  response.render('home')
+app.get('/', async (request, response) => {
+  const db = await dbConnection;
+  const categorias = await db.all('SELECT * FROM categorias;');
+  response.render('home', {
+    categorias
+  })
 });
 
 app.get('/vaga', (request, response) => {
   response.render('vaga')
 });
+
+const init = async() => {
+  const db = await dbConnection;
+  await db.run('CREATE TABLE if not exists categorias (id INTEGER PRIMARY KEY, categoria TEXT);');
+  const categoria = 'Engineering team'
+  await db.run(`INSERT INTO categorias(categoria) VALUES('${categoria}')`);
+}
+init();
 
 app.listen(3000, 'localhost', (err) => {
   if(err) {
