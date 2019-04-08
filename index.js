25b015e3d0908ace802c7bcfe18d3e87209b6116
marcuspereiragithub <vinnycoast@gmail.com>
2019-04-08 10:52:33 -0300

    Create table for vacancies

--- index.js
+++ index.js
@@ -2,28 +2,41 @@ const express = require('express');
 const app = express();
 
 const sqlite = require('sqlite');
-const dbConnection = sqlite.open('banco.sqlite', { Promise });
+const dbConnection = sqlite.open('db.sqlite', { Promise });
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', async (request, response) => {
   const db = await dbConnection;
-  const categorias = await db.all('SELECT * FROM categorias;');
+  const categoriesDb = await db.all('SELECT * FROM categories;');
+  const vacancies = await db.all('SELECT * FROM vacancies;');
+
+  const categories = categoriesDb.map(cat => {
+    return {
+      ...cat,
+      vacancies: vacancies.filter(vacancy => vacancy.category === cat.id)
+    }
+  });
+  
   response.render('home', {
-    categorias
+    categories
   })
 });
 
-app.get('/vaga', (request, response) => {
-  response.render('vaga')
+app.get('/vacancy', (request, response) => {
+  response.render('vacancy')
 });
 
 const init = async() => {
   const db = await dbConnection;
-  await db.run('CREATE TABLE if not exists categorias (id INTEGER PRIMARY KEY, categoria TEXT);');
-  const categoria = 'Engineering team'
-  await db.run(`INSERT INTO categorias(categoria) VALUES('${categoria}')`);
+  await db.run('CREATE TABLE if not exists categories (id INTEGER PRIMARY KEY, category TEXT);');
+  await db.run('CREATE TABLE if not exists vacancies (id INTEGER PRIMARY KEY, category INTEGER, title TEXT, description TEXT);');
+  // const category = 'Marketing team'
+  // await db.run(`INSERT INTO categories(category) VALUES('${category}')`);
+  // const vacancy = 'Digital Marketing (San Francisco)';
+  // const description = 'Vacancy to Marketing team'
+  // await db.run(`INSERT INTO vacancies(category, title, description) VALUES(2, '${vacancy}', '${description}')`);
 }
 init();
 
