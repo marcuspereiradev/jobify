5ed46e915aff82ff9d88aa9f5533c6221fa68ab1
marcuspereiragithub <vinnycoast@gmail.com>
2019-04-10 09:39:01 -0300

    Ensuring that NOW finds my views

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const sqlite = require('sqlite');
 const dbConnection = sqlite.open(path.resolve(__dirname, 'db.sqlite'), { Promise });
 const port = process.env.PORT || 3000
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true}));
